034836f86d410478d04198d5b54d674d95d7c69f
santibrizuela <santibrizuela.97@gmail.com>
2024-08-08 19:57:49 -0300

    changed strive version

--- app/api/create-payment-intent/route.ts
+++ app/api/create-payment-intent/route.ts
@@ -5,7 +5,7 @@ import { CartProductType } from "@/app/product/[productId]/ProductDetails";
 import { getCurrentUser } from "@/actions/getCurrentUser";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string,{
-    apiVersion: "2024-04-10",
+    apiVersion: "2024-06-10",
 });
 
 const calculateOrderAmount = (items: CartProductType[]) => {
